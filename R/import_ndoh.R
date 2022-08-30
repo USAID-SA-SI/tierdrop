ed7eb5765ad86c99209e51c3eaa10a0577dc4c97
Karishma Srikanth <krs8rb@virginia.edu>
2022-08-30 11:11:56 +0200

    add initial NDOH read function

---
+++ R/import_ndoh.R
@@ -0,0 +1,53 @@
+#' Import NDOH tabs
+#'
+#' @param qtr return the quarter of the NDOH reporting file
+#' @param kp processes KP tabs if TRUE and non-KP tabs if FALSE (default kp = FALSE)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    ndoh_all <- import_ndoh(qtr = "Q3", kp = FALSE)
+#'    ndoh_kp <- import_ndoh(qtr = "Q3", kp = TRUE)
+#' }
+#'
+
+import_ndoh <- function(qtr, kp = FALSE) {
+
+  if (qtr %in% c("Q1", "Q3") & kp == FALSE) {
+    indic_list <- tier_qtr
+  } else if (qtr == "Q2" & kp == FALSE) {
+    indic_list <- tier_semi
+  } else if (qtr == "Q4" & kp == FALSE) {
+    indic_list <- tier_ann
+  } else if (kp == TRUE) {
+    indic_list <- tier_kp_indic
+  }
+
+  #Read in TIER with new function
+  ndoh_all <- indic_list %>%
+    purrr::map_dfr(.f = ~ read_all_the_things(ndoh_filepath, sheet = .x)) %>%
+    dplyr::relocate(`Test Result/Outcome/Duration`, .after = Code) %>%
+    dplyr::relocate(Result, .after = CoarseAgeGroup)
+
+
+  ndoh_all <- ndoh_all %>%
+    dplyr::mutate(District = dplyr::recode(District,
+                                           "fs Thabo Mofutsanyana District Municipality" = "fs Thabo Mofutsanyane District Municipality")) %>%
+    dplyr::filter(District %in% usaid_dsp_district) %>%
+    dplyr::mutate(code_num = stringr::str_length(Code)) %>%
+    dplyr::group_by(Province, District, SubDistrict, Facility) %>%
+    dplyr::arrange(desc(code_num)) %>%
+    dplyr::left_join(mfl_new_df %>% dplyr::select(OU5name, Old_OU5Code), by = c("Facility" = "OU5name")) %>%
+    dplyr::mutate(Old_OU5Code = as.character(Old_OU5Code),
+                  Code = ifelse(code_num < 6, Old_OU5Code, Code)) %>%
+    tidyr::fill(Code) %>%
+    dplyr::ungroup() %>%
+    #  dplyr::count(Facility, Code) %>%
+    dplyr::select(-c(code_num, Old_OU5Code))
+
+return(ndoh_all)
+}
+
+
--- data-raw/setup-processing-script.R
+++ data-raw/setup-processing-script.R
@@ -1,12 +1,20 @@
 # SETUP script
-
 dir_setup()
 
-#Load MFL
+#set folderpaths
+folderpath <- 'data-raw/NDOH'
+ndoh_filepath <- folderpath %>% glamr::return_latest()
 
+#Load MFL as is
+mfl_new_df <- googlesheets4::read_sheet(mfl_new_id)
+
+#then grab the clean MFL
 df_fac <- clean_mfl()
 
-# folderpath <- 'data-raw/NDOH'
-# filepath <- folderpath %>% glamr::return_latest()
-#
-# read_all_the_things(filepath, "TX_CURR")
+
+# IMPORT NDOH
+
+#first for non-KP indics
+ndoh_all <- import_ndoh(qtr = "Q3", kp = FALSE)
+
+ndoh_kp <- import_ndoh(qtr = "Q3", kp = TRUE)
