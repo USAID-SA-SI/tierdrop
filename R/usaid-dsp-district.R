ccb0ba47b66d3b2baafc1b66254b8681a1980776
Karishma Srikanth <krs8rb@virginia.edu>
2022-08-30 11:11:33 +0200

    add global var for USAID dsp PSNUs

---
+++ R/usaid-dsp-district.R
@@ -0,0 +1,22 @@
+#' USAID South Africa Districts
+#' @description
+#' This is the list of PSNUs that USAID South Africa DSPs work in (length = 15)
+#'
+#' @export
+#'
+
+usaid_dsp_district <- c("ec Alfred Nzo District Municipality",
+                    "ec Buffalo City Metropolitan Municipality",
+                    "fs Lejweleputswa District Municipality",
+                    "fs Thabo Mofutsanyane District Municipality",
+                    "gp City of Johannesburg Metropolitan Municipality",
+                    "gp Sedibeng District Municipality",
+                    "kz Harry Gwala District Municipality",
+                    "kz King Cetshwayo District Municipality",
+                    "kz Ugu District Municipality",
+                    "lp Capricorn District Municipality",
+                    "lp Mopani District Municipality",
+                    "mp Ehlanzeni District Municipality",
+                    "mp Gert Sibande District Municipality",
+                    "mp Nkangala District Municipality",
+                    "wc City of Cape Town Metropolitan Municipality")
