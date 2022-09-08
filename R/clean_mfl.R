d5c4bc94349bb55ab576c8753368cb803af7cd00
Karishma Srikanth <krs8rb@virginia.edu>
2022-09-08 23:37:24 +0200

    add commented out code to amend MFL to bring in mech - come back to this

--- R/clean_mfl.R
+++ R/clean_mfl.R
@@ -28,5 +28,13 @@ clean_mfl <- function() {
   return(df_fac)
 }
 
-
+# mfl_new_df %>%
+#   dplyr::filter(!is.na(OU2name)) %>%
+#   janitor::clean_names() %>%
+#   dplyr::select(ou5name, ou5uid, datim_uid, mechanism_i_d, mechanism_uid, old_ou5code, tidyselect::starts_with("fy")) %>%
+#   tidyr::pivot_longer(cols = tidyselect::starts_with("fy22"), names_to = "period", values_to = "DSD_TA") %>%
+#   dplyr::mutate(period = stringr::str_sub(period, start = 1, end = 6) %>% toupper(),
+#                 old_ou5code = as.character(old_ou5code),
+#                 DSD_TA = ifelse(DSD_TA == "DSD+Roving TA", "DSD", DSD_TA)) %>%
+#   dplyr::rename(usaid_facility = ou5name)
 
