6bb192f47c262b7975a2ffc96b6e3b02e3324043
Karishma Srikanth <krs8rb@virginia.edu>
2023-10-26 23:23:21 -0400

    updates to script

--- data-raw/FY23Q4_processing.R
+++ data-raw/FY23Q4_processing.R
@@ -81,3 +81,148 @@ mech_mfl <- mfl_new_df %>%
 #get DSD/TA breakdown
 df_fac <- clean_mfl() %>%
   rename(DSD_TA = dsd_ta)
+
+#adjust TX_TB_D names for now
+names_tx_tb_d <- c(standard_names, "CoarseAgeGroup",  "Sex", "Result","Total")
+
+# NDOH ---------------------------------------------------------------------
+
+#if this breaks, check on tab names (adjust PrEP_NEW in the file itself)
+# update for ou5uid
+ndoh_all <- import_ndoh(filepath = ndoh_filepath, qtr = curr_qtr, kp = FALSE)
+ndoh_all_kp <- import_ndoh(filepath = ndoh_filepath, qtr = curr_qtr, kp = TRUE)
+
+
+#what facilities are in NDOH but not in MFL? ADdress MFL qc as needed
+# update for ou5uid
+validate_ndoh(ndoh_all)
+validate_ndoh(ndoh_all_kp)
+
+# TIDY NDOH -----------------------------------------------------------
+
+#TIDY
+ndoh_clean <- tidy_ndoh(ndoh_all, kp = FALSE) %>%
+  select(-c(Code))
+
+#aggregate TB_STAT
+ndoh_tb_stat <- ndoh_clean %>%
+  filter(indicator == "TB_STAT",
+         numeratordenom == "N",
+         `Test Result/Outcome/Duration` %in% c("New Negative", 'New Positive')) %>%
+  #filter(CoarseAgeGroup == "<1")
+  mutate(CoarseAgeGroup = ifelse(CoarseAgeGroup =="<1", "1-4", CoarseAgeGroup)) %>%
+  group_by(usaid_facility, ou5uid, datim_uid, new_ou5_code, period, DSD_TA, Province, SubDistrict, District, Facility, `Test Result/Outcome/Duration`,
+           Sex, CoarseAgeGroup, Result, indicator, numeratordenom) %>%
+  dplyr::summarise(dplyr::across(tidyselect::starts_with("Total"), \(x) sum(x, na.rm = TRUE)), .groups = "drop")
+
+ndoh_clean <-  ndoh_clean %>%
+  filter(!(indicator == "TB_STAT" & numeratordenom == "N" &
+             `Test Result/Outcome/Duration` %in% c("New Negative", 'New Positive'))) %>%
+  rbind(ndoh_tb_stat)
+
+ndoh_clean_kp <- tidy_ndoh(ndoh_all_kp, kp = TRUE) %>%
+  select(-c(Code))
+
+# MAP -------------------------------------------------------------------------------------------
+
+#Map dataelements and mechs
+df_mapped <- ndoh_post_processing(ndoh_clean %>% filter(!(indicator == "TX TB_Denom" & numeratordenom == "D")),
+                                  kp = FALSE, export_type = "Validation")
+
+df_mapped_kp <- ndoh_post_processing(ndoh_clean_kp, kp = TRUE, export_type = "Validation")
+
+# do a check to see what is not getting mapped
+  #only TX_TB indics before weird - filter out for now
+  #6 sites with missing sex / other disaggs for TX indicators
+df_mapped %>%
+  distinct() %>%
+  filter(is.na(dataElement),
+         indicator %ni% c("TX_TB_Denom", "TX_TB_Numer"))
+
+df_mapped_kp %>%
+  distinct() %>%
+  filter(is.na(dataElement)) %>%
+  count(indicator)
+
+
+#bind together and filter out those that did not have mappings
+df_final <- dplyr::bind_rows(df_mapped,
+                             df_mapped_kp) %>%
+  distinct() %>%
+  filter(!is.na(dataElement)) %>%
+  mutate(mech_code = as.character(mech_code))
+
+df_final %>%
+  janitor::get_dupes()
+
+# CLEAN UP ------------------------------------------------------------
+
+#Step 1: Filter out PrEP for Harry Gwala, Capricorn and Mopani; filter out all of MATCH PrEP for now
+df_final_clean <- df_final %>%
+  filter(!(District == "kz Harry Gwala District Municipality" & indicator %in% c("PrEP_CT", "PrEP_NEW"))) %>%
+  filter(!(District %in% c("lp Capricorn District Municipality",
+                           "lp Mopani District Municipality")
+           & indicator %in% c("PrEP_CT", "PrEP_NEW"))) %>%
+  filter(!(mech_code == "81902" & indicator %in% c("PrEP_CT", "PrEP_NEW")))
+
+
+# Step 2: Keep only certain sites for MATCH prep sites
+match_prep_q2 <- reference_folder %>%
+  return_latest("PrEP Facilities MatCH Q2 reporting") %>%
+  read_excel() %>%
+  janitor::clean_names()
+
+
+#grab uids for prep MATCH sites
+match_prep_uids <- match_prep_q2 %>%
+  # filter(prep_facility == "YES") %>%
+  left_join(df_fac %>% select(usaid_facility, datim_uid),
+            by = c("supported_facilities" = "usaid_facility")) %>%
+  distinct(datim_uid) %>%
+  pull()
+
+
+#MATCH PREP IMPORT FILE
+import_MATCH_prep <- df_final %>%
+  filter((orgUnit_uid %in% match_prep_uids
+          & indicator %in% c("PrEP_CT", "PrEP_NEW"))) %>%
+  mutate(period = import_period_style)
+
+
+# BIND WITH REST ---------------------------------------------------------
+#for partner review
+tier_final_partner <- bind_rows(df_final_clean %>% select(all_of(partner_vars)),
+                                #tb_all_final%>% select(all_of(partner_vars)),
+                                import_MATCH_prep %>% select(all_of(partner_vars))
+) %>%
+  mutate(period = import_period_style) %>%
+  filter(!is.na(orgUnit_uid), #beatty mobile 5 and senorita hospital
+         !is.na(mech_uid))
+
+#for import file
+tier_final_import <- bind_rows(df_final_clean %>% select(all_of(import_vars)),
+                               #  tb_all_final%>% select(all_of(import_vars)),
+                               import_MATCH_prep %>% select(all_of(import_vars))) %>%
+  mutate(period = import_period_style) %>%
+  filter(!is.na(orgUnit_uid), #beatty mobile 5 and senorita hospital
+         !is.na(mech_uid))
+
+#check for dupes
+tier_final_import %>%
+  select(import_vars) %>%
+  janitor::get_dupes()
+
+#EXPORT
+today <- lubridate::today()
+
+tier_final_import %>%
+  readr::write_csv(glue::glue("{import_folder}/{fiscal_quarter}_TIER_Import_File_{today}.csv"))
+
+
+#Partner files
+Broadreach_import <- partner_import(df = tier_final_partner, 70287)
+RTC_import <- partner_import(df = tier_final_partner, 70290)
+ANOVA_import <- partner_import(df = tier_final_partner, 70310)
+MATCH_import <- partner_import(df = tier_final_partner, 81902)
+WRHI_import <- partner_import(df = tier_final_partner, 70301)
+
