81b91bcabc8660eb4fc75f63cb63d9c5eb672b12
Karishma Srikanth <krs8rb@virginia.edu>
2024-01-29 02:24:21 -0500

    add google id for fy24 mfl

--- R/gs_id.R
+++ R/gs_id.R
@@ -7,6 +7,14 @@
 
 mfl_new_id <- "1UuDYK4X-Lr8avq4IUsKGWChr17AE4Bc21MlHO3OWB-Y"
 
+#' FY24 Master Facility List Google Sheet ID
+#'
+#' @export
+#' @return google sheet id
+#' @keywords internal
+
+mfl_fy24_id <- "1whfC1a9Aga6UZNleuNO8n4orLUeeuu5Ew59OpnXFqwc"
+
 #' FY23 Master Facility List Google Sheet ID
 #'
 #' @export
