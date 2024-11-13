37901dc56d5282bc9fbdebedf3e776b2e5a29dbd
Srikanth <ksrikanth@usaid.gov>
2024-11-13 17:40:15 -0500

    update clean MFL

--- R/clean_mfl.R
+++ R/clean_mfl.R
@@ -10,7 +10,7 @@
 #'
 #'   mfl <- clean_mfl()
 #' }
-clean_mfl <- function(mfl_period = period) {
+clean_mfl <- function(mfl_period = period, type = "main") {
 
   #use metadata from get_meta()
   tab_name <- str_c("MFL_", fy, "_", curr_qtr)
@@ -18,17 +18,30 @@ clean_mfl <- function(mfl_period = period) {
   #Read in MFL from google drive (need to parameterize this for the sheet name)
   mfl_new_df <- googlesheets4::read_sheet(mfl_fy24_id, sheet = tab_name)
 
-  #Reshape facility list - for Fy22Q3 list, change to account for DSD/Roving TA
-  df_fac <- mfl_new_df %>%
-    dplyr::filter(!is.na(OU2name)) %>%
-    janitor::clean_names() %>%
-    dplyr::select(ou5name, ou5uid, datim_uid, new_ou5_code, tidyselect::contains("dsd")) %>%
-    dplyr::rename("dsd_ta" = glue::glue("{fiscal_quarter %>% tolower()}_dsd_ta")) %>%
-    # tidyr::pivot_longer(cols = tidyselect::contains("dsd"), names_to = "period", values_to = "DSD_TA") %>%
-    dplyr::mutate(period = fiscal_quarter,
-                  new_ou5_code = as.character(new_ou5_code),
-                  dsd_ta = ifelse(dsd_ta == "DSD+Roving TA", "DSD", dsd_ta)) %>%
-    dplyr::rename(usaid_facility = ou5name)
+  if (type == "main") {
+
+    df_fac <- mfl_new_df %>%
+      dplyr::filter(!is.na(OU2name)) %>%
+      janitor::clean_names() %>%
+      dplyr::select(ou5name, ou5uid, datim_uid, new_ou5_code, tidyselect::contains("dsd")) %>%
+      dplyr::rename("dsd_ta" = glue::glue("{fiscal_quarter %>% tolower()}_dsd_ta")) %>%
+      dplyr::mutate(period = fiscal_quarter,
+                    new_ou5_code = as.character(new_ou5_code),
+                    dsd_ta = ifelse(dsd_ta == "DSD+Roving TA", "DSD", dsd_ta)) %>%
+      dplyr::rename(usaid_facility = ou5name)
+
+  } else if (type == "mech") {
+    #get mech info from MFL
+    df_fac <- mfl_new_df %>%
+      dplyr::filter(!is.na(OU2name)) %>%
+      janitor::clean_names() %>%
+      dplyr::select(ou5name, datim_uid,partner, mechanism_i_d, mechanism_uid) %>%
+      dplyr::rename(sitename = ou5name,
+                    facilityuid = datim_uid,
+                    prime_partner_name = partner,
+                    mech_code = mechanism_i_d,
+                    mech_uid = mechanism_uid)
+  }
 
   return(df_fac)
 }
